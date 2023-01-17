22e9368df9ba80d7aef358793d1d20dd5cf4c0fc
Sherwood <bsherwood@gmail.com>
2023-01-17 15:53:18 -0600

    update
    update

--- notUsed/handBoot/handBoot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=ben.sherwood@ku.edu
-#SBATCH --partition=sherwood
-#SBATCH --time=21-00:00:00
-#SBATCH --mem=5000
-module load R 
-R CMD BATCH --vanilla handBoot.r
--- notUsed/hetero/TheoremSimHeteroXYBig5.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=ben.sherwood@ku.edu
-#SBATCH --partition=sherwood
-#SBATCH --time=14-00:00:00
-#SBATCH --mem=32000
-module load R 
-R CMD BATCH --vanilla TheoremSimHeteroXYBig5.r
